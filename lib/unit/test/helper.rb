cbd2ed6a839fdcadf7d8c1403986c77fb60e3d6a
刘思兰 <liusilan@douyu.tv>
2021-03-05 16:44:45 +0800

    init

---
+++ lib/unit/test/helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "helper/version"
+
+module Unit
+  module Test
+    module Helper
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/unit/test/helper/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Unit
+  module Test
+    module Helper
+      VERSION = "0.1.0"
+    end
+  end
+end
